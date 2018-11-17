044547174e1a6fe73c508857c7706be8f440c2d2
pedro <pedro.sanchezxp@hotmail.com>
2018-11-17 15:54:01 -0500

    Making new activities

---
+++ FINAL_TD_PROJECT_1/app/src/main/java/com/example/antoniobrito/td_challenge_project/augmentedimage/chequingAccActivity.java
@@ -0,0 +1,13 @@
+package com.example.antoniobrito.td_challenge_project.augmentedimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chequingAccActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chequing_acc);
+    }
+}
